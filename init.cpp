9e2da94e31a3a35f4933977df8b80eb74f4445f0
mattinstruluz <30941518+terzinodipaese@users.noreply.github.com>
2019-10-11 23:36:51 -0700

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -16,9 +16,15 @@ template <typename T> void init(T *v, int len, T value)
 	memcpy(v+size, v, (len-size)*4);
 }
 
+//That takes O(n) time
+template <typename T> void _init(T *v, int len, T value)
+{
+	for(int i=0; i<len; i++)
+		v[i] = value;
+}
+
 int main()
 {
-	
 	
 	return 0;
 }
